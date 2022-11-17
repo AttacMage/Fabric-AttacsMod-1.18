df860869497c937f24d324b6c6b66269f1286c70
AttacMage <spacem4gee@gmail.com>
2022-11-17 03:05:46 -0700

    Initial commit

---
+++ src/main/java/net/attacmage/attacsmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.attacmage.attacsmod.mixin;
+
+import net.attacmage.attacsmod.AttacsMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		AttacsMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
